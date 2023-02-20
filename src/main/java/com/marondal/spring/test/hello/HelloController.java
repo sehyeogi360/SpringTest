589be1250a5cde29cf8ed75cd34e26ea99f47fee
DESKTOP-61D76IS\user <forthewy@gmail.com>
2023-02-20 18:31:46 +0900

    [임세혁] -SpringTest최초 커밋

---
+++ src/main/java/com/marondal/spring/test/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.marondal.spring.test.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HelloController {
+	 @ResponseBody
+	    @RequestMapping("/hello")
+	    public String helloWorld() {
+	        return "Hello World!!";
+	    }
+}
